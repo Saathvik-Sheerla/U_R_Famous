62b61b8fa30418dd558a85e7920ff6892c6c117b
Saathvik-Sheerla <140387813+Saathvik-Sheerla@users.noreply.github.com>
2023-07-27 19:28:57 +0530

    app.py

--- app.py
+++ app.py
@@ -6,24 +6,85 @@ rn = st.number_input("Enter your roll_no ")
 
 if rn == 30:
     st.title("_YOU ARE DHEERAJ_")
+    st.write("You are from K.K.R.H.S")
+    st.write("You fear ZOMBIES")
+    st.write("You like Traffic Rider")
+    st.subheader("Oopsuu")
+    st.write("fav food : Biryani")
 
-if rn == 1:
+if rn == 12:
     st.title("_YOU ARE ABHIRAM_")
+    st.write("You fear your brother")
+    st.write("You like mini militia")
+    st.subheader("Abhi")
+    st.write("fav food : Biryani")
 
-if rn == 2:
+if rn == 23:
     st.title("_YOU ARE SAANVI_")
+    st.write("You are from K.K.R.H.S")
+    st.write("You fear lizard")
+    st.write("You like Vlogs,tours")
+    st.subheader("Milky")
+    st.write("fav food : peanuts")
 
-if rn == 3:
+if rn == 19:
     st.title("_YOU ARE ANIRUDH_")
+    st.write("You fear Mom")
+    st.write("You like making videos, indian bike racer")
+    st.subheader("Anir,Chitri")
+    st.write("fav food : Chicken")
 
-if rn == 4:
+if rn == 8:
     st.title("_YOU ARE HARSHINI SAGAR IAS_")
+    st.write("You are from K.K.R.P.S")
+    st.write("You fear Nothing , universe extinction")
+    st.write("You like Play games,Writing")
+    st.subheader("Papa,Ramana,Mr,Nookayya,papuu,papaayya,pops")
+    st.write("fav food : Egg")
 
-if rn == 5:
+if rn == 26:
     st.title("_YOU ARE HARSHITH_")
+    st.write("You are from K.K.R.P.S")
+    st.write("You fear Varshith")
+    st.write("You like Horror shows")
+    st.subheader("Bingg")
+    st.write("fav food : Aloo fry")
 
-if rn == 6:
+if rn == 33:
     st.title("_YOU ARE VARSHITH_")
+    st.write("You are from K.K.R.Pp.S")
+    st.write("You fear Absolutely nothing")
+    st.write("You like cycle")
+    st.subheader("Siggo,Varshu,varshu kanna")
+    st.write("fav food : chicken , mutton , talkay , boati")
 
-if rn == 7:
-    st.title("_YOU ARE CHARAN_")
+if rn == 29:
+    st.title("_YOU ARE D CHARAN_")
+    st.write("You are from K.K.R.H.S")
+    st.write("Your pet's name is KIWI")
+    st.write("Best friend : DHEERAJ")
+
+if rn == 13:
+    st.title("_YOU ARE NAGA SANJANA_")
+    st.write("You are from K.K.R.H.S")
+    st.write("Best friend : SAANVI")
+
+if rn == 16:
+    st.title("_YOU ARE RISHITHA_")
+    st.write("You are from K.K.R.P.S")
+    st.write("Your pet's name is KIWI")
+    st.write("Best friend : HARSHINI")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
